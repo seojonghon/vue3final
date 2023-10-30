4dfb56b0233791940ed7fd555f889f293647f547
seojonghon <seojonghon@naver.com>
2023-10-30 20:50:45 +0900

    first

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://54.210.254.103:8080',
         // changeOrigin: true,
         // pathRewrite: {
         //   '^/': ''
